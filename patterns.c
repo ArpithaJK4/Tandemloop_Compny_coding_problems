e6561794645c82cfc341316a4a38bb4775d9fe91
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-01-27 02:21:45 -0800

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void printPattern();
+
+int main() {
+    printPattern();
+    return 0;
+}
+
+void printPattern() {
+    int rows = 5;
+
+    for (int i = 0; i < rows; i++) {
+
+        for (int j = 0; j < i; j++) {
+            printf("");
+        }
+
+        int num = (i % 2 == 0) ? 1 : i + 1;
+        for (int k = 0; k <= i; k++) {
+            if (i != 0 && k != 0) {
+                printf(" # %d", num);
+                num = (i % 2 == 0) ? num + 1 : num - 1;
+            } else {
+                printf(" %d", num);
+                num = (i % 2 == 0) ? num + 1 : num - 1;
+            }
+        }
+        printf("\n");
+    }
+}
